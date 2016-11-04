08ca452f51a4d63b40c5d3f2fa4ee1c3d7e699c4
Paul Amonson <paul.d.amonson@intel.com>
2016-11-04 14:23:47 -0700

    Fixed the UT for utilities.ping_check by mocking subprocess.call.
    Change-Id: Id469e483c9f0ce4a99a4fe5e860475503ef05855
    Signed-off-by: Paul Amonson <paul.d.amonson@intel.com>

--- utilities/tests/test_utilities.py
+++ utilities/tests/test_utilities.py
@@ -7,16 +7,25 @@ Tests for the OS and Network Utilities
 """
 import getpass
 import unittest
+import subprocess
 from ctrl.utilities.utilities import Utilities
 
 
 class TestGetOsUtilities(unittest.TestCase):
     """These should work even under docker simulation"""
-    @unittest.skip("The ping command is failing in docker")
+
+    def mocked_call(self, args, stdin=None, stdout=None, stderr=None,
+                    shell=False):
+        """Fake a call to subprocess.call"""
+        return 0
+
     def test_get_os_utilities(self):
         """All tests are in this method."""
         utilities = Utilities()
+        saved = subprocess.call
+        subprocess.call = self.mocked_call
         self.assertEqual(True, utilities.ping_check("127.0.0.1"))
+        subprocess.call = saved
         rv = utilities.execute_no_capture(['ls', '/someunknownrootfolder'])
         self.assertEqual(2, rv)
         rv = utilities.execute_with_capture(['ls', '/someunknownrootfolder'])
