4735d66b7284be79d71931e4673316e7df94a6c9
dstott <devyn.stott@intel.com>
2016-11-09 13:49:23 -0800

    feat(): fix import schemes by add the package to the path.
    Change-Id: I0e88e068787d5076f5b8aa0f2e3be987b290e24a
    Signed-off-by: dstott <devyn.stott@intel.com>

--- ctrlcli.py
+++ ctrlcli.py
@@ -13,8 +13,15 @@
 Main Module to call Control CLI.
 """
 
-
-from cli.control_cli import CtrlCliExecutor
-
 if __name__ == '__main__':
+    # If we are running this as a cli file, then edit the path to
+    # contain the directory the file is in.
+    if __package__ is None:
+        import sys
+        from os import path
+
+        sys.path.append(path.dirname(path.dirname(path.realpath(__file__))))
+
+    from cli.control_cli import CtrlCliExecutor
+
     CtrlCliExecutor.execute_cli_cmd()
