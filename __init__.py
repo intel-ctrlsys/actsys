740fa93e71b0950503838d8ca8b4fe27acf34a71
Aakash N Patel <aakash.n.patel@intel.com>
2016-11-09 10:03:43 -0800

    Fixing the __init__.py in ctrl root directory to fix the build issue.
    Change-Id: I32046e73266b7f92290d29dc448a9e46a38015d1
    Signed-off-by: Aakash N Patel <aakash.n.patel@intel.com>

--- __init__.py
+++ __init__.py
@@ -2,11 +2,3 @@
 #
 # Copyright (c) 2016 Intel Corp.
 #
-
-from bmc import *
-from cli import *
-from commands import *
-from os_remote_access import *
-from plugin import *
-from power_control import *
-from utilities import *
