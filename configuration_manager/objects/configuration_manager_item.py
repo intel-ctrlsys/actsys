90dca1d672324aef505df2dc6136aaa69795dd54
Victor Armengol <victor.armengol.fernandez@intel.com>
2016-11-16 15:32:37 -0600

    Moving the behavior of Device to a more general class
    Change-Id: Ia0641bd397394c44f67454ca2e355647a77ee079
    Signed-off-by: Victor Armengol <victor.armengol.fernandez@intel.com>

---
+++ configuration_manager/objects/configuration_manager_item.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2016 Intel Corp.
+#
+"""Module that defines a read only object with dynamic number of attributes"""
+
+
+class ConfigurationManagerItem(object):
+    """ Base class of a Configuration  Manager Object"""
+
+    def __init__(self, attributes=None):
+        """ Init function, you can only add a dictionary of attributes
+         at this step.
+        :type attributes: dict
+        """
+        if attributes is not None:
+            self.__update_dict__(attributes)
+
+    def __update_dict__(self, new_attributes):
+        """ Function to update internal dictionary
+        :type new_attributes: dict
+        :param new_attributes: dictionary to add attributes or update values
+        """
+        self.__dict__.update(new_attributes)
+
+    def __add_attribute__(self, name, value):
+        """Allows to add an attribute to an existent object"""
+        new_attribute = dict({name: value})
+        self.__update_dict__(new_attribute)
+
+    def get_attribute_list(self):
+        """Allow access to the list of attributes of this object"""
+        return vars(self).keys()
+
+    def __repr__(self):
+        """ Transfer the object's representation to the one of __dict__"""
+        return self.__dict__.__repr__()
+
+    def __getattr__(self, item):
+        """ Getattr function, if we try to access to a non existent attribute
+        then we must return None
+        """
+        return None
+
+    def __setattr__(self, key, value):
+        """ Overrides setattr function so attributes can be read-only """
+        pass
--- configuration_manager/objects/device.py
+++ configuration_manager/objects/device.py
@@ -3,37 +3,23 @@
 # Copyright (c) 2016 Intel Corp.
 #
 """ Module that defines base clase for cluster representation objects """
+from ctrl.configuration_manager.objects.configuration_manager_item \
+    import ConfigurationManagerItem
 
 
-class Device(object):
+class Device(ConfigurationManagerItem):
     """ Base class to represent a device in a cluster configuration """
 
     def __init__(self, attributes=None):
-        """ Init function """
-        if attributes is not None:
-            self.__update_dict__(attributes)
+        """ Init function
+        :type attributes: dict
+        """
+        if not attributes:
+            attributes = dict({})
 
-    def __update_dict__(self, new_attributes):
-        """ Function to update internal dictionary """
-        self.__dict__.update(new_attributes)
+        if 'device_name' not in attributes:
+            attributes['device_name'] = None
 
-    def __add_attribute__(self, attribute_name, attribute_value):
-        """Allows to add an attribute"""
-        new_attribute = dict({attribute_name: attribute_value})
-        self.__update_dict__(new_attribute)
-
-    def get_attribute_list(self):
-        """Allow access to the list of attributes of this object"""
-        return vars(self).keys()
-
-    def __repr__(self):
-        """ Transfer the object's representation to the one of __dict__"""
-        return self.__dict__.__repr__()
-
-    def __getattr__(self, item):
-        """ Getattr function """
-        return None
-
-    def __setattr__(self, key, value):
-        """ Overrides setattr function so attributes can be read-only """
-        pass
+        if 'device_type' not in attributes:
+            attributes['device_type'] = None
+        super(Device, self).__init__(attributes)
