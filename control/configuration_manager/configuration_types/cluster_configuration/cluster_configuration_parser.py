277d467da2622d02a526ddf7e64431b85593e509
Minia Oseguera <minia.oseguera@intel.com>
2016-11-25 16:02:57 -0600

    [Configuration Manager] Add functionality to expand lists
    Change-Id: I364cd0da61b2cf2be9df04c6fb7e5a219d522f33
    Signed-off-by: Victor Armengol <victor.armengol.fernandez@intel.com>
    Signed-off-by: Minia Oseguera <minia.oseguera@intel.com>

--- control/configuration_manager/configuration_types/cluster_configuration/cluster_configuration_parser.py
+++ control/configuration_manager/configuration_types/cluster_configuration/cluster_configuration_parser.py
@@ -4,35 +4,79 @@
 #
 """Implementation of a cluster configuration parser"""
 from __future__ import print_function
+import re
 from ...json_parser.json_parser import JsonParser
 from .cluster_configuration_data import ClusterConfigurationData
 from ...objects.device import Device
 
-def update_bmc(device, bmc):
-    """ Updates  bmc attribute in a device object """
-    if device:
-        device.__update_dict__(dict(bmc=bmc))
 
-def update_power_outlet_list(device, info, attribute_name):
-    """ Updates the list in attribute name with the given info """
-    if not device or not info or not attribute_name:
-       return
-    if device.__dict__.get(attribute_name):
-        device.__dict__[attribute_name].append(info)
-    else:
-        device.__update_dict__({attribute_name:[info]})
+class ListExpander(object):
+    """ Expands a list in a string """
+
+    expandable_list_pattern = r',|\[\d:\d+-\d+\]'
+    list_pattern = r'(?P<LIST>\[(?P<LENGTH>\d+):(?P<START>\d+)-(?P<END>\d+)\])'
+    regex_expandable_list = re.compile(expandable_list_pattern)
+    regex_list_pattern = re.compile(list_pattern)
+
+    @classmethod
+    def build_item_list(cls, head, length, start, end, tail):
+        """Build a list with a defined range, and a head and tail strings"""
+        result_list = []
+        for digit in range(start, end+1):
+            token = "{:s}{:0{:d}d}{:s}".format(head, digit, length, tail)
+            result_list.extend(cls.expand_item(token))
+        return result_list
+
+    @classmethod
+    def expand_item(cls, token):
+        """Expand an item returning a list with the expansions"""
+        result_list = []
+        token_substrings = cls.regex_list_pattern.split(token, 1)
+        if len(token_substrings) is 1:
+            result_list.append(token_substrings[0])
+        else:
+            head, _, length, start, end, tail = token_substrings
+            result_list.extend(cls.build_item_list(head,
+                                                   int(length),
+                                                   int(start),
+                                                   int(end),
+                                                   tail))
+        return result_list
+
+    @classmethod
+    def is_expandable(cls, string):
+        """ Indicates if a string can be expanded or not """
+        return cls.regex_expandable_list.search(string)
+
+    @classmethod
+    def expand_list(cls, compressed_list):
+        """ Receives a string with a list inside and expands that list
+            returning a list of the resultant strings"""
+        result_list = []
+        token_list = compressed_list.replace(' ', '').split(',')
+        for token in [x for x in token_list if len(x) > 0]:
+            result_list.extend(cls.expand_item(token))
+        return result_list
 
 
 class ClusterConfigurationParser(object):
     """ Cluster Configuration Parser """
+
     known_ids = ['device_id', 'hostname', 'ip_address']
     known_types = {
-        'NODE_TAG':'node',
-        'BMC_TAG':'bmc',
-        'PDU_TAG':'pdu',
-        'PSU_TAG':'psu',
-        'CONFIG_VARS_TAG':'configuration_variables'
+        'NODE_TAG': 'node',
+        'BMC_TAG': 'bmc',
+        'PDU_TAG': 'pdu',
+        'PSU_TAG': 'psu',
+        'CONFIG_VARS_TAG': 'configuration_variables'
     }
+    __hostname_pattern = r'^([0-9]*[a-zA-Z]|' \
+                         r'[0-9]*[a-zA-Z][a-zA-Z0-9\-]*[a-zA-Z0-9])$'
+    __regex_hostname = re.compile(__hostname_pattern)
+    __ip_pattern = r'^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.)' \
+                   r'{3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$'
+    __regex_ip = re.compile(__ip_pattern)
+
     def __init__(self, file_path):
         self.file_parser = JsonParser()
         self.data = self.file_parser.read_file(file_path)
@@ -87,9 +131,15 @@ class ClusterConfigurationParser(object):
         for outlet in device.connected_device:
             relationship_info = (device.device_id, outlet.get('outlet'))
             for connected_device in outlet.get('device', []):
-                target_device = self.parsed_data.search_device(connected_device)
-                update_power_outlet_list(target_device, relationship_info,
-                                         attribute_name)
+                if ListExpander.is_expandable(connected_device):
+                    connected_device_list = \
+                        ListExpander.expand_list(connected_device)
+                else:
+                    connected_device_list = [connected_device]
+                for device_id in connected_device_list:
+                    target_device = self.parsed_data.search_device(device_id)
+                    update_power_outlet_list(target_device, relationship_info,
+                                             attribute_name)
 
     def print_parse_data(self):
         """ Prints parsed data object """
@@ -126,7 +176,7 @@ class ClusterConfigurationParser(object):
             self.__parse_item__(device_type, item, True)
 
     def __parse_item__(self, device_type, item, is_item_list=False):
-        self.__replace_profile(item)
+        self.__replace_profile__(item)
         if device_type in self.known_types.values():
             self.__parse_known_item__(device_type, item)
         else:
@@ -139,10 +189,6 @@ class ClusterConfigurationParser(object):
         if device_id:
             self.__store_item__(device_type, device_id, item)
 
-    def __is_expandable(self, item):
-        # todo implement this
-        return False
-
     def __remove_all_device_id__(self, profile):
         for id_attribute in self.known_ids:
             profile.pop(id_attribute, None)
@@ -153,7 +199,7 @@ class ClusterConfigurationParser(object):
                 return item[id_attribute]
         return None
 
-    def __replace_profile(self, item):
+    def __replace_profile__(self, item):
         profile = self.profiles.get(item.pop('profile', None))
         if profile:
             profile = profile.copy()
@@ -161,21 +207,32 @@ class ClusterConfigurationParser(object):
             item.update(profile)
 
     def __store_group_item__(self, group):
-        item_list = self.expand_group(group)
+        item_list = expand_group(group)
         for item in item_list:
-            self.__store_item__(item['device_type'], item['device_id'], item)
-
-    def expand_group(self, group):
-        # todo make this actually work
-        return [group]
+            self.__add_device__(item)
 
     def __store_item__(self, device_type, device_id, item):
         item['device_type'] = device_type
         item['device_id'] = device_id
-        if self.__is_expandable(item):
+        if ListExpander.is_expandable(device_id):
             self.__store_group_item__(item)
         else:
-            self.parsed_data.add_device(Device(item))
+            self.__add_device__(item)
+
+    def __add_device__(self, item):
+        if item.get('hostname') and \
+            not self.__is_valid_hostname__(item.get('hostname')):
+            return
+        if item.get('ip_address') and \
+            not self.__is_valid_ip_addr__(item.get('ip_address')):
+            return
+        self.parsed_data.add_device(Device(item))
+
+    def __is_valid_hostname__(self, hostname):
+        return self.__regex_hostname.match(hostname) is not None
+
+    def __is_valid_ip_addr__(self, ip_addr):
+        return self.__regex_ip.match(ip_addr) is not None
 
     def __parse_unknown__(self, device_type, item, is_item_list):
         device_id = self.__find_id__(item)
@@ -185,3 +242,84 @@ class ClusterConfigurationParser(object):
             self.__store_item__(device_type, device_id, item)
         else:
             self.__store_item__(device_type, device_type, item)
+
+
+def update_bmc(device, bmc):
+    """ Updates  bmc attribute in a device object """
+    if device:
+        device.__update_dict__(dict(bmc=bmc))
+
+
+def update_power_outlet_list(device, info, attribute_name):
+    """ Updates the list in attribute name with the given info """
+    if not device or not info or not attribute_name:
+        return
+    if device.__dict__.get(attribute_name):
+        device.__dict__[attribute_name].append(info)
+    else:
+        device.__update_dict__({attribute_name: [info]})
+
+
+def get_min_length(lists):
+    """ List of lists,
+    returns the length of the shorter list. """
+    if not lists:
+        return 0
+    min_length = len(lists[0])
+    for list_ in lists:
+        if len(list_) < min_length:
+            min_length = len(list_)
+    return min_length
+
+
+def fix_lists_length(lists_dict):
+    """ @param lists_dict - Dictionary of lists
+    This function will fix the length of all the lists inside
+    the given dictionary to the length of the shorter list.
+    """
+    if not lists_dict:
+        return
+    min_length = get_min_length(lists_dict.values())
+    for attribute in lists_dict:
+        lists_dict[attribute] = \
+            lists_dict[attribute][:min_length]
+
+
+def create_group_items(base_item, expanded_attributes):
+    """ This function will create a list of items
+    based on the given base_item and append or update
+    the attributes given in expanded_attributes dictionary
+    for each of the items that will be created.
+    """
+    result_list = []
+    if not expanded_attributes.get('device_id'):
+        return result_list
+    for index in xrange(len(expanded_attributes.get('device_id'))):
+        item = base_item.copy()
+        for attribute in expanded_attributes:
+            item[attribute] = expanded_attributes[attribute][index]
+        result_list.append(item)
+    return result_list
+
+
+def expand_attributes(group, attributes_list):
+    """ For each attribute in group (included in attributes_list),
+    this function will expand its content in case it represents a list. """
+    expanded_attributes = {}
+    if attributes_list:
+        for attribute in attributes_list:
+            if group.get(attribute):
+                expanded_attributes[attribute] = \
+                    ListExpander.expand_list(group[attribute])
+    return expanded_attributes
+
+
+def expand_group(group):
+    """" Expands attributes for group and create a list of items
+    with those attributes """
+    if not group:
+        return []
+    expanded_attributes = expand_attributes(group, \
+        ['device_id', 'bmc', 'hostname', 'ip_address'])
+    fix_lists_length(expanded_attributes)
+    return create_group_items(group, expanded_attributes)
--- control/configuration_manager/configuration_types/cluster_configuration/tests/test_cluster_configuration_parser.py
+++ control/configuration_manager/configuration_types/cluster_configuration/tests/test_cluster_configuration_parser.py
@@ -6,16 +6,52 @@
 """ Tests for ClusterConfigurationParser Module """
 
 from unittest import TestCase
-from ..cluster_configuration_parser import ClusterConfigurationParser
+from ..cluster_configuration_parser \
+    import ClusterConfigurationParser, expand_group
+
 
 class TestClusterConfigurationParser(TestCase):
     """ Tests for ClusterConfigurationData Module """
+
     def setUp(self):
         """ Setup function"""
         self.conf_file = \
             "control/configuration_manager/json_parser/tests/file.json"
         self.parser = ClusterConfigurationParser(self.conf_file)
         self.parser.parse()
+
+    def test_expand_goup_different_len(self):
+        group = {'device_id':'compute-[1:29-32]', 'device_type':'node',
+                 'someattr':'hello', 'ip_address': '192.168.2.[1:29-32]',
+                 'bmc':'bmc[1:1-3]'}
+        expected = [{'device_id':'compute-29', 'device_type':'node',
+                     'someattr':'hello', 'ip_address': '192.168.2.29',
+                     'bmc':'bmc1'},
+                    {'device_id':'compute-30', 'device_type':'node',
+                     'someattr':'hello', 'ip_address': '192.168.2.30',
+                     'bmc':'bmc2'},
+                    {'device_id':'compute-31', 'device_type':'node',
+                     'someattr':'hello', 'ip_address': '192.168.2.31',
+                     'bmc':'bmc3'}]
+        self.assertEqual(expand_group(group), expected)
+
+    def test_expand_goup_(self):
+        group = {'device_id':'compute-[1:29-32]', 'device_type':'node',
+                 'someattr':'hello', 'ip_address': '192.168.2.[1:29-32]',
+                 'hostname':'compute-[1:29-32]'}
+        expected = [{'device_id':'compute-29', 'device_type':'node',
+                     'someattr':'hello', 'ip_address': '192.168.2.29',
+                     'hostname':'compute-29'},
+                    {'device_id':'compute-30', 'device_type':'node',
+                     'someattr':'hello', 'ip_address': '192.168.2.30',
+                     'hostname':'compute-30'},
+                    {'device_id':'compute-31', 'device_type':'node',
+                     'someattr':'hello', 'ip_address': '192.168.2.31',
+                     'hostname':'compute-31'},
+                    {'device_id':'compute-32', 'device_type':'node',
+                     'someattr':'hello', 'ip_address': '192.168.2.32',
+                     'hostname':'compute-32'}]
+        self.assertEqual(expand_group(group), expected)
 
     def test__set_node_relationships__(self):
         self.assertEquals(self.parser.parsed_data['node']['master4'].bmc,
@@ -48,4 +84,52 @@ class TestClusterConfigurationParser(TestCase):
                           None)
         self.assertEquals(
             self.parser.parsed_data['pdu']['192.168.3.32'].psu_list, None)
+
+    def test_is_valid_hostname(self):
+        valid_strings = ['a',
+                         'ab',
+                         '0a',
+                         '00a',
+                         '00a0-2-a',
+                         'qwww',
+                         'wWw',
+                         'my-www',
+                         '3www',
+                         'host-5512']
+
+        not_valid_strings = ['cosa_sad',
+                             'my www',
+                             'mu-',
+                             'mu_',
+                             'my_www',
+                             '-www',
+                             '5512',
+                             '@www',
+                             'something.some.othersome.com']
+
+        for test_string in valid_strings:
+            self.assertTrue(self.parser.__is_valid_hostname__(test_string),
+                            'True expected for {0}'.format(test_string))
+        for test_string in not_valid_strings:
+            self.assertFalse(self.parser.__is_valid_hostname__(test_string),
+                             'False expected for {0}'.format(test_string))
+
+    def test_is_valid_ip(self):
+        valid_strings = ['0.0.0.0',
+                         '192.165.4.8',
+                         '255.255.255.255']
+
+        not_valid_strings = ['-1.25.65.45',
+                             '192.168.1.2.5',
+                             '256.168.1.1',
+                             '192.256.4.8',
+                             '192.165.256.8',
+                             '192.165.4.256',
+                             '1922.165.4.250'
+                             '192.165.4.1111'
+                             '']
+
+        for test_string in valid_strings:
+            self.assertTrue(self.parser.__is_valid_ip_addr__(test_string),
+                            'True expected for {0}'.format(test_string))
 
