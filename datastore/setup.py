0f358a09b797d0593490f56fa92f3f0e33f8de78
Ke Wang <ke1.wang@intel.com>
2017-07-17 15:42:43 -0700

    Update the travis yml file for travis CI build
    Signed-off-by: Ke Wang <ke1.wang@intel.com>

--- datastore/setup.py
+++ datastore/setup.py
@@ -26,7 +26,7 @@ setup(name='ctrlsys-datastore',
       entry_points={
           'console_scripts': ['datastore = datastore.__main__:main']
       },
-      install_requires=['psycopg2', 'python-dateutil>=2.6.0', 'pytz', 'clustershell'],
+      install_requires=['psycopg2', 'python-dateutil>=2.6.0', 'pytz'],
       test_suite='tests',
       tests_require=tests_require,
       extras_require={
